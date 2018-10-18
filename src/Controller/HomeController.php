32106d4be950d2c7fb8714eb0e4e757426ee5e15
Jesus Ibarra <jose.ibarra@ula.edu.mx>
2018-10-18 16:28:46 -0500

    Se agregó dump

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -21,8 +21,38 @@ class HomeController extends AbstractController
      * @Route("/estudiante/{variable}", name="estudiante")
      */
     public function estudiante($variable){
-            return new Response(sprintf(
+           /* return new Response(sprintf(
             	"Portal del estudiante: %s", $variable
-            ));
+            ));*/
+        $comentarios = [
+                'comentario1',
+                'comentario2',
+                'comentario3'
+        ];
+        $matriculas = [
+        		'U991234',
+        		'U995678',
+        		'U999876',
+        		'U999879'
+        ];
+       
+        return $this->render('home/show.html.twig',[
+ 			'titulo' => ucwords(str_replace('-', ' ', $variable)),
+ 			'contenido' => ucwords(str_replace('-', ' ', 'Está es una prueba symfony')),
+ 			'comentarios' =>  $comentarios,
+ 			'matriculas' => $matriculas
+        ]);
+
     }
+
+     /**
+     * @Route("/contacto/", name="contacto")
+     */
+    public function contacto(){
+    	return $this->render('contacto/contacto.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+  
 }
